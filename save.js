db22dad8628e996d849e724b43a6c3dbc7c05ae6
Workx46 <forworkx46@gmail.com>
2022-08-31 16:17:03 +0800

    new

--- save.js
+++ save.js
@@ -1,1 +1,14 @@
+for(var i = 0; i < robots.length; i++){
+    if (robots[i].score < 220){
+        if (robots.length > 1) robots.splice(i, 1);
+        if (GUIs.length > 1) {
+            GUIs[i].hide();
+            GUIs.splice(i, 1);
+        }
+        if (i >= robots.length) i = robots.length - 1;
+        updateLocalParams(i);
+        winner = 0;
+        winnerScore = 9999;
+    }
+}
 setTimeout(saveEvent, 25000)
